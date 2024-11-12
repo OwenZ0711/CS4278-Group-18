af289cedebf27bc09205b76c4dd0e7431e08c306
SihanYe <sihan.ye@vanderbilt.edu>
2024-11-12 08:22:59 -0600

    change redirect URI

--- backend/InfoKey.py
+++ backend/InfoKey.py
@@ -1,6 +1,6 @@
 CLIENT_ID = '5de8da9998ff4436abab42f799e1476c' # trail clients id
 CLIENT_SECRET_ID = 'ed07ca9837c240fea4b153a56566f3b0'
-REDIRECT_URI = 'https://frontend2-sihanye-sihan-yes-projects.vercel.app/loading'
+REDIRECT_URI = 'https://backendtest3-b4ff149de3c9.herokuapp.com/loading'
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
